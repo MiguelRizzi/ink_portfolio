d31864354646f446392635d8cadb72de0a363b78
Miguel Angel Rizzi <123975192+MiguelRizzi@users.noreply.github.com>
2024-02-26 02:30:49 -0300

    Add portfolio:management view

--- project/apps/portfolio/urls.py
+++ project/apps/portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='portfolio/index.html'), name='index'),
+    path('management/', views.PortfolioManagementView.as_view(), name='management'),
 ]
