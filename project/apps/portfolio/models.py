544c25eed8671469348c0c922c2f79893507442f
Miguel Angel Rizzi <123975192+MiguelRizzi@users.noreply.github.com>
2024-02-26 17:43:55 -0300

    Add model Message

--- project/apps/portfolio/models.py
+++ project/apps/portfolio/models.py
@@ -18,3 +18,15 @@ class Design(models.Model):
 
     def __str__(self):
         return f"{self.title} - available ={self.available}"
+    
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=100)
+    message = models.TextField()
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
