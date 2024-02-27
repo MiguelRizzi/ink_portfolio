6f39e7db02828f5e0e4b068f2299530520f4568d
Miguel Angel Rizzi <123975192+MiguelRizzi@users.noreply.github.com>
2024-02-27 18:18:38 -0300

    Add model "Review"

--- project/apps/reviews/models.py
+++ project/apps/reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Review(models.Model):
+    RATING_CHOICES = [
+        (1, '1 - Malo'),
+        (2, '2 - Regular'),
+        (3, '3 - Promedio'),
+        (4, '4 - Bueno'),
+        (5, '5 - Excelente'),
+    ]
+    name = models.TextField(max_length=100)
+    email = models.EmailField()
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    review = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
