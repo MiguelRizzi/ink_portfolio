48cc7c6bb9a3c2c3122457fffa352f60c3acfe98
Miguel Angel Rizzi <123975192+MiguelRizzi@users.noreply.github.com>
2024-03-08 19:29:50 -0300

    AvatarDeleteView updated, add delete confirm modal

--- project/apps/users/urls.py
+++ project/apps/users/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path('change_password/', views.CustomPasswordChangeView.as_view(), name='change_password'),
-    path("avatar/detail/<int:pk>", views.AvatarDetail.as_view(), name="avatar_detail"),
-    path("avatar/create/", views.AvatarCreate.as_view(), name="avatar_create"),
-    path("avatar/update/<int:pk>", views.AvatarUpdate.as_view(), name="avatar_update"),
-    path("avatar/delete/<int:pk>", views.AvatarDelete.as_view(), name="avatar_delete"),
+    path("avatar/detail/<int:pk>", views.AvatarDetailView.as_view(), name="avatar_detail"),
+    path("avatar/create/", views.AvatarCreateView.as_view(), name="avatar_create"),
+    path("avatar/update/<int:pk>", views.AvatarUpdateView.as_view(), name="avatar_update"),
+    path("avatar/delete/<int:pk>", views.AvatarDeleteView.as_view(), name="avatar_delete"),
 ]
--- project/apps/users/views.py
+++ project/apps/users/views.py
@@ -1,44 +1,49 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.views import PasswordChangeView
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
+from django.views import View
 from django.views.generic import DetailView
-from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .forms import AvatarForm
 from .models import Avatar
-from django.contrib import messages
-from django.contrib.auth.views import PasswordChangeView
 
 
 # Create your views here.
-class AvatarDetail(LoginRequiredMixin, DetailView):
+class AvatarDetailView(LoginRequiredMixin, DetailView):
     model = Avatar
     
-class AvatarCreate(LoginRequiredMixin, CreateView):
+class AvatarCreateView(LoginRequiredMixin, CreateView):
     model = Avatar
     form_class = AvatarForm
-    success_url = reverse_lazy('portfolio:index')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         messages.success(self.request, "El avatar se guardó correctamente.", extra_tags="alert alert-success")
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        return reverse('users:avatar_detail', kwargs={'pk': self.object.pk})
    
     
-class AvatarUpdate(LoginRequiredMixin, UpdateView):
+class AvatarUpdateView(LoginRequiredMixin, UpdateView):
     model= Avatar
     form_class = AvatarForm
     success_url = reverse_lazy('portfolio:index')
     def form_valid(self, form):
         messages.success(self.request, "El avatar se actualizó correctamente.", extra_tags="alert alert-success")
         return super().form_valid(form)
-
-    
-class AvatarDelete(LoginRequiredMixin, DeleteView):
-    model= Avatar
-    success_url = reverse_lazy("portfolio:index")
     def get_success_url(self):
-        messages.success(self.request, "El Avatar se eliminó correctamente.", extra_tags="alert alert-danger")
-        return super().get_success_url()
+        return reverse('users:avatar_detail', kwargs={'pk': self.object.pk})
+    
+class AvatarDeleteView(LoginRequiredMixin, View):
+    def get(self, request, pk):
+        avatar = get_object_or_404(Avatar, pk=pk)
+        avatar.delete()
+        messages.success(request, "El avatar se eliminó correctamente.", extra_tags="alert alert-danger")
+        return redirect('portfolio:index')
+    
     
 class CustomPasswordChangeView(PasswordChangeView):
     template_name="users/change_password.html"
